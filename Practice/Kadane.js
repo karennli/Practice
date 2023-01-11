4363962e55ee84307904ea9d51f4557d4e5c950f
Karen <karennli2016@gmail.com>
2023-01-10 19:33:10 -0500

    passed tests

--- Practice/Kadane.js
+++ Practice/Kadane.js
@@ -1,1 +1,18 @@
-//testing push to github
+function kadanesAlgorithm(array) {
+  let maxSums = [];
+
+  for(let i in array){
+    let max = [array[i]]; //[-9, -4, ]
+    let pointer = i-1;   //0
+    while(pointer>=0){
+      //find all sums of subarray ending at i 
+        max.push(max[max.length-1]+array[pointer]); 
+        pointer--; 
+    }
+   maxSums.push(Math.max(...max));
+  }
+  return Math.max(...maxSums);
+}
+
+//should output 19
+console.log(kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]));
