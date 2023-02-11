70f67b53eca10d60f8510d1e0274bdfc7960697e
Karen <karennli2016@gmail.com>
2023-02-10 23:43:12 -0500

    without mutating sequence

--- Practice/ValidSub.js
+++ Practice/ValidSub.js
@@ -1,8 +1,9 @@
 function isValidSubsequence(array, sequence) {
+    let changedSequence = [...sequence]
     for (let i of array) {
-        if (i === sequence[0]) { sequence.shift(); }
+        if (i === changedSequence[0]) { changedSequence.shift(); }
     }
-    return sequence.length === 0
+    return changedSequence.length === 0
 }
 
 console.log(isValidSubsequence([1, 2, 3, 4], [1, 3, 4]))
