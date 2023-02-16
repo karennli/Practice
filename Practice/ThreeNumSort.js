8a182064993516d0e8705dd6eabecc1836307f64
Karen <karennli2016@gmail.com>
2023-02-15 21:54:07 -0500

    changed count var

--- Practice/ThreeNumSort.js
+++ Practice/ThreeNumSort.js
@@ -1,12 +1,12 @@
 function threeNumberSort(array, order) {
-    let [c1, c2, c3] = [0, 0, 0]
+    let counts = [0, 0, 0]
     for (let num of array) {
-        if (num === order[0]) { c1++ };
-        if (num === order[1]) { c2++ };
-        if (num === order[2]) { c3++ };
+        if (num === order[0]) { counts[0]++ };
+        if (num === order[1]) { counts[1]++ };
+        if (num === order[2]) { counts[2]++ };
     }
-    array.fill(order[0], 0, c1);
-    array.fill(order[1], c1, c1 + c2);
-    array.fill(order[2], c1 + c2)
+    array.fill(order[0], 0, counts[0]);
+    array.fill(order[1], counts[0], counts[0] + counts[1]);
+    array.fill(order[2], counts[0] + counts[1])
     return array;
 }
