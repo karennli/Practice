c4c7ae5edeaa8b4c226d4bdc97d51ba628f121c7
Karen <karennli2016@gmail.com>
2023-03-18 23:57:15 -0400

    product sum refactor

--- Practice/ProductSum.js
+++ Practice/ProductSum.js
@@ -1,12 +1,12 @@
 function productSum(array, depth = 1) {
     let sum = 0;
-    for (let i = 0; i <= array.length - 1; i++) {
-        if (Array.isArray(array[i])) {
-            const deepSum = productSum(array[i], depth + 1)
+    for (let i of array) {
+        if (Array.isArray(i)) {
+            const deepSum = productSum(i, depth + 1)
             sum += (depth + 1) * deepSum
         }
         else {
-            sum += array[i];
+            sum += i;
         }
     }
     return sum;
